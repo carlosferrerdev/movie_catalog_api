fe39ab8ffc19484718d8ec442354431f954f6dc7
Carlos Ferrer <carlosvferrer@gmail.com>
2023-04-08 19:15:13 -0300

    Tests created to the API

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:genre) }
+    it { should validate_presence_of(:year) }
+    it { should validate_presence_of(:published_at) }
+    it { should validate_presence_of(:description) }
+
+    it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
+    it { should validate_length_of(:description).is_at_least(10).is_at_most(2000) }
+
+    it { should validate_numericality_of(:year).only_integer }
+    it { should validate_numericality_of(:year).is_greater_than_or_equal_to(1800).is_less_than_or_equal_to(Date.today.year) }
+  end
+end
