86130462f6d9c78a72e4e0febac21bba1a608f35
Carlos Ferrer <carlosvferrer@gmail.com>
2023-04-08 11:34:46 -0300

    Create validations in the model movie.rb file

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  validates :id, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :genre, precense: true
+  validates :year, presence: true
+  validates :country, presence: true
+  validates :published_at, presence: true
+  validates :description, presence: true
 end
