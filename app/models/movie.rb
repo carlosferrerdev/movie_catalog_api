47d122f44257fc81e2824e1c5fe669b688baef14
Carlos Ferrer <carlosvferrer@gmail.com>
2023-04-09 13:12:57 -0300

    Bug fixed in the description validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1, maximum: 255 }
-  validates :genre, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :genre, presence: true, length: { minimum: 1, maximum: 255 }
   validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1800, less_than_or_equal_to: Date.today.year }
   validates :published_at, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 2000 }
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Movie, type: :model do
     it { should validate_presence_of(:description) }
 
     it { should validate_length_of(:title).is_at_least(1).is_at_most(255) }
+    it { should validate_length_of(:genre).is_at_least(1).is_at_most(255) }
     it { should validate_length_of(:description).is_at_least(10).is_at_most(2000) }
 
     it { should validate_numericality_of(:year).only_integer }
